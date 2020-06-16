9f1a29f7ac0b97212606b0969efb9c98d2c7522b
MuhammadFahad97 <muhammadfahad97bukx@gmail.com>
2020-06-16 17:40:04 +0500

    commit 5:39

--- TestingGit/TestingGit/MySecondForm.Designer.cs
+++ TestingGit/TestingGit/MySecondForm.Designer.cs
@@ -28,34 +28,47 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.MyButton = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// MyButton
-			// 
-			this.MyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.MyButton.Location = new System.Drawing.Point(50, 44);
-			this.MyButton.Name = "MyButton";
-			this.MyButton.Size = new System.Drawing.Size(154, 65);
-			this.MyButton.TabIndex = 0;
-			this.MyButton.Text = "Click Me!!! ";
-			this.MyButton.UseVisualStyleBackColor = true;
-			this.MyButton.Click += new System.EventHandler(this.MyButton_Click);
-			// 
-			// MySecondForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(255, 157);
-			this.Controls.Add(this.MyButton);
-			this.Name = "MySecondForm";
-			this.Text = "MySecondForm";
-			this.ResumeLayout(false);
+            this.MyButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MyButton
+            // 
+            this.MyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MyButton.Location = new System.Drawing.Point(50, 44);
+            this.MyButton.Name = "MyButton";
+            this.MyButton.Size = new System.Drawing.Size(154, 65);
+            this.MyButton.TabIndex = 0;
+            this.MyButton.Text = "Click Me!!! ";
+            this.MyButton.UseVisualStyleBackColor = true;
+            this.MyButton.Click += new System.EventHandler(this.MyButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MySecondForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 157);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MyButton);
+            this.Name = "MySecondForm";
+            this.Text = "MySecondForm";
+            this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.Button MyButton;
-	}
+        private System.Windows.Forms.Button button1;
+    }
 }
