3c771a0e329bc76dea6f2494885908785b200cce
TAYYAB DOST <tayyabdost@gmail.com>
2020-06-16 17:32:20 +0500

    Second Form Added

---
+++ TestingGit/TestingGit/MySecondForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestingGit
+{
+	public partial class MySecondForm : Form
+	{
+		public MySecondForm()
+		{
+			InitializeComponent();
+		}
+
+		private void MyButton_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("This is my Second Form and I build it during your work", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information); 
+		}
+	}
+}
