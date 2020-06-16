f7ab36ecfb4ae65a3dfe65381259a5bac0ac6cd2
MuhammadFahad97 <muhammadfahad97bukx@gmail.com>
2020-06-16 18:11:20 +0500

    commit 6:11

--- TestingGit/TestingGit/MySecondForm.Designer.cs
+++ TestingGit/TestingGit/MySecondForm.Designer.cs
@@ -35,7 +35,7 @@
             // MyButton
             // 
             this.MyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MyButton.Location = new System.Drawing.Point(50, 44);
+            this.MyButton.Location = new System.Drawing.Point(51, 12);
             this.MyButton.Name = "MyButton";
             this.MyButton.Size = new System.Drawing.Size(154, 65);
             this.MyButton.TabIndex = 0;
@@ -45,11 +45,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(89, 122);
+            this.button1.Location = new System.Drawing.Point(51, 83);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(154, 62);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Click 2";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -57,6 +57,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(255, 157);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.MyButton);
